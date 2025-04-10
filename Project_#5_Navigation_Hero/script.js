d0d3d31e09ad1f67b27cc65b99e5fb24c7de27a8
haidersafi530 <haidersafi2000@gmail.com>
2025-04-10 10:56:54 +0500

    Navigation Hero Section with animation   Apr 10 - 10:56 AM

--- Project_#5_Navigation_Hero/script.js
+++ Project_#5_Navigation_Hero/script.js
@@ -1,14 +1,52 @@
 let active = 3;
 let mncircle = document.querySelectorAll(".minicircle");
-gsap.to(mncircle[active-1], {
-    opacity : 1
-})
+let sec = document.querySelectorAll(".sec");
 
-mncircle.forEach(function(val){
-    val.addEventListener('click', function(){
-        alert('Hi');
-    })
-})
+// Set initial state
+gsap.to(mncircle[active - 1], {
+    opacity: 0.5
+});
+gsap.to(sec[active - 1], {
+    opacity: 1
+});
+
+mncircle.forEach(function(val, index) {
+    val.addEventListener('click', function() {
+        // Animate the circle rotation
+        gsap.to("#circle", {
+            rotate: (3 - (index + 1)) * 10,
+            ease: "expo.inOut",
+            duration: 1
+        });
+
+        // Fade all to grey first
+        greyout();
+
+        // Highlight the clicked circle
+        gsap.to(mncircle[index], {
+            opacity: 0.5
+        });
+
+        // Highlight the corresponding section
+        gsap.to(sec[index], {
+            opacity: 1
+        });
+
+        // Update active index if needed
+        active = index + 1;
+    });
+});
+
+// Function to fade all to grey
+function greyout() {
+    gsap.to(mncircle, {
+        opacity: 0.08
+    });
+
+    gsap.to(sec, {
+        opacity: 0.4
+    });
+}
 
 gsap.to("#circle", {
     rotate : 0,
